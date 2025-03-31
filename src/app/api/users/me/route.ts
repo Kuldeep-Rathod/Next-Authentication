93e8c2323e04d360937a06f81556a67646fab3f2
Kuldeep Rathod <kuldiprathod375@gmail.com>
2025-03-31 17:48:18 +0530

    add my profile api

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,35 @@
+import { dbConnect } from "@/config/dbConnect";
+import User from "@/models/userModel";
+import { getDataFromToken } from "@/utils/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+
+dbConnect();
+
+export const POST = async (req: NextRequest) => {
+    try {
+        const userId = await getDataFromToken(req);
+
+        const user = await User.findOne({ _id: userId }).select("-password");
+
+        if (!user) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "User Not Found",
+                },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json({
+            success: true,
+            message: "User Found",
+            data: user,
+        });
+    } catch (error: any) {
+        return NextResponse.json({
+            success: false,
+            error: error.message,
+        });
+    }
+};
---
+++ src/utils/getDataFromToken.ts
@@ -0,0 +1,17 @@
+import { dbConnect } from "@/config/dbConnect";
+import jwt from "jsonwebtoken";
+import { NextRequest } from "next/server";
+
+dbConnect();
+
+export const getDataFromToken = async (req: NextRequest) => {
+    try {
+        const token = req.cookies.get("token")?.value || "";
+
+        const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
+
+        return decodedToken.id;
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+};
